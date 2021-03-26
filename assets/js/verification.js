73a27e4939400aa310cd9769c75dbed14cf8fb8b
Fabian Conejo <fabianconejosegu@gmail.com>
2021-03-25 22:03:36 -0600

    Change interface and styles, defined new form to validate user login information.

--- assets/js/verification.js
+++ assets/js/verification.js
@@ -27,19 +27,7 @@ const account1 = new Account("maui", "maui123", 500);
 const account2 = new Account("dany", "dany123", 800);
 const account3 = new Account("gus", "gus123", 1000);
 
-// Getting autentication params from query string
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-
-const username = urlParams.get("username").toLowerCase();
-const password = urlParams.get("password").toLowerCase();
-
 // Validating Data
-
-const home = () => {
-    window.location = "./home.html";
-}
 
 const invalidInfo = () => {
 
@@ -53,23 +41,23 @@ const invalidInfo = () => {
 }
 
 const authenticate = () => {
-    if (username === account1.username && password === account1.password || username === account2.username && password === account2.password || username === account3.username && password === account3.password) {
-        home();
-    } else if (username && password){
+
+    const user = document.getElementById("username").value.toLowerCase();
+    const pass = document.getElementById("password").value.toLowerCase();
+
+    if (user === account1.username && pass === account1.password ) {
+
+
+    } else if (user === account2.username && pass === account2.password) {
+
+
+    } else if (user === account3.username && pass === account3.password) {
+
+
+    } else {
         invalidInfo();
-
-        const uri = window.location.toString();
-
-        if (uri.indexOf("?") > 0) {
-        const clean_uri = uri.substring(0, uri.indexOf("?"));
-        window.history.replaceState({}, document.title, clean_uri);
-}
     }
 }
-
-// Calling function to authenticate
-
-authenticate()
 
 
 
